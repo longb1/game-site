387061987a2855d8d706a4fc47700feaecb17e87
Long <17272827+longb1@users.noreply.github.com>
2022-04-11 00:26:02 +0100

    Add styling and functionality to buttons
    Layout the drums in a row and select them in JavaScript to add functionality on key press down.

---
+++ instrument/drum_script.js
@@ -0,0 +1,26 @@
+main=document.body
+allKeys = document.querySelectorAll('.keys')
+allKeys.forEach(element => {
+    element.addEventListener("keydown",press=>{
+        
+        switch(e.keycode){
+            case 65: //A
+                //play sound whatever
+            case 83:
+    
+            case 68:
+    
+            case 70:
+    
+            case 71:
+    
+            case 72:
+    
+            case 74:
+    
+            case 75:
+    
+            case 76: //L
+        }
+    })
+});
