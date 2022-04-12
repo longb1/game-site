baff33237511cd173e13f6e5e31fb1c6ce49401d
Long <17272827+longb1@users.noreply.github.com>
2022-04-12 10:10:48 +0100

    Add drum kit and sounds
    Add drum sounds and associate them to DOM elements to play on each key press. This will complete the logic for my drumkit project. Just need to improve styling.

--- instrument/drum_script.js
+++ instrument/drum_script.js
@@ -1,26 +1,31 @@
-main=document.body
+const main=document.body
 allKeys = document.querySelectorAll('.keys')
-allKeys.forEach(element => {
-    element.addEventListener("keydown",press=>{
-        
-        switch(e.keycode){
-            case 65: //A
-                //play sound whatever
-            case 83:
-    
-            case 68:
-    
-            case 70:
-    
-            case 71:
-    
-            case 72:
-    
-            case 74:
-    
-            case 75:
-    
-            case 76: //L
-        }
-    })
+let KeyA = document.getElementById('a-key')
+const KeyS= document.getElementById('s-key')
+const KeyD= document.getElementById('d-key')
+const KeyF= document.getElementById('f-key')
+const KeyG= document.getElementById('g-key')
+const KeyH= document.getElementById('h-key')
+const KeyJ= document.getElementById('j-key')
+const KeyK= document.getElementById('k-key')
+const KeyL= document.getElementById('l-key')
+
+
+document.addEventListener('keydown', function (e){
+
+    const sound=document.querySelector(`audio[data-key="${e.code}"]`)
+    const key=document.querySelector(`.keys[data-key="${e.code}"]`)
+    if (!sound) return "that aint a key";
+    sound.currentTime=0;
+    sound.play();
+    key.classList.add('drum-hit');
 });
+
+function removeTransition(e){
+    // console.log(e)
+    // if(e.propertyName!=="border") return; //if not border transitioning then skip
+    this.classList.remove('drum-hit')
+};
+
+const keys=document.querySelectorAll('.keys')
+keys.forEach(keyz => keyz.addEventListener('transitionend',removeTransition));
