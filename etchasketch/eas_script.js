5929643d7d20060897930dbc4b8b341a3d057242
Long <17272827+longb1@users.noreply.github.com>
2022-04-14 23:02:43 +0100

    Add code logic for eas
    add code logic and style layout to a 16x16 grid with red borders.

--- etchasketch/eas_script.js
+++ etchasketch/eas_script.js
@@ -1,7 +1,48 @@
 canvasElement =document.getElementById("canvas")
 
-for(let i =0;i<256;i++){
-    let squareDiv = document.createElement("div"); 
-    squareDiv.classList.add("finalBlock");
-    canvasElement.appendChild(squareDiv);
+var isToggling = false;
+
+function enableToggle(e) {
+  console.log('enableToggle')
+  isToggling = true;
+
+  if (e.target !== table) {
+    toggle(e);
+  }
 }
+
+function disableToggle() {
+  console.log('disableToggle')
+  isToggling = false;
+}
+
+function toggle(e) {
+  if (isToggling === false) {
+    return;
+  }
+
+  console.log('toggle:', e.target);
+
+  e.target.classList.toggle('active');
+}
+
+function startGame() {
+  table.onmousedown = enableToggle;
+
+  for (var i = 0, il = tableEntry.length; i < il; i++) {
+    tableEntry[i].onmouseenter = toggle; //changes color 
+  }
+
+  table.onmouseup = disableToggle;
+}
+
+startGame();
+
+for(let i =1;i<=256;i++){
+    let squareDiv = document.createElement("div");
+    // squareDiv.innerText=i; 
+    canvasElement.appendChild(squareDiv);
+    squareDiv.addEventListener('mousedown', toggle(startDrawing));
+}
+
+
