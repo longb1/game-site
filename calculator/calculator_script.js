4e2010e47557dadedc201d1d6ba5462c2b093634
Long <17272827+longb1@users.noreply.github.com>
2022-04-16 11:22:48 +0100

    Add clear button and functionality
    the clear button will now remove all values from display when clicked. this will enable users to reset if they make an error

--- calculator/calculator_script.js
+++ calculator/calculator_script.js
@@ -1,10 +1,11 @@
 const display=document.getElementById("contDisplay");
 const numberContainer=document.getElementById("contNumbers");
 const operatorContainer=document.getElementById("contOperators");
-const equalsButton=document.getElementById("calculate");
 const operatorButtons=operatorContainer.children;
+const equalsButton=document.getElementById("calculate");
+const clearButton=document.getElementById("clear");
 
-for(let i =0;i<=9;i++){ //add number button elements to number container
+for(let i =0;i<=9;i++){ //create number buttons 0-9
     let numButton = document.createElement("button");
     numButton.innerText=i;
     numberContainer.appendChild(numButton);
@@ -14,10 +15,12 @@ for(let i =0;i<=9;i++){ //add number button elements to number container
 
 
 
-for(let i=0;i<operatorButtons.length;i++){
+for(let i=0;i<operatorButtons.length;i++){ //append the 'data-operator' attribute of buttons to the display div.
     operatorButtons[i].addEventListener("click",()=>display.innerText+=operatorButtons[i].dataset.operator)
 }
 
 equalsButton.addEventListener("click",function calculate(){
-    console.log("results here");
+    console.log(display.innerText);
 })
+
+clearButton.addEventListener("click",()=>display.innerText="")
