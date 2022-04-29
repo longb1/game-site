b4c7eb33b08e48b2ba74a1b9311cd3884665dde2
Long <17272827+longb1@users.noreply.github.com>
2022-04-29 18:17:12 +0100

    Add button to screen
    Add button for user to click which prompts for a book title

---
+++ library/lib_script.js
@@ -0,0 +1,32 @@
+const addBookBtn=document.getElementById("bookBtn"); //get button
+const library = document.getElementById("container");
+let myLibrary = []; 
+
+addBookBtn.addEventListener('click',displayBookOnPage);
+
+
+class Book {
+  constructor(author, title, pageNumber, hasBeenRead) {
+    this.author = author;
+    this.title = title;
+    this.pageNumber = pageNumber;
+    this.hasBeenRead = hasBeenRead;
+  }
+}
+
+function addBookToLibrary(){
+    const userInput=prompt('Enter a book into your library.');
+    myLibrary.push(userInput);
+    console.log(myLibrary);
+    return userInput
+}
+
+function displayBookOnPage(){
+    const book=addBookToLibrary() //get user input
+    let newTab= document.createElement("div"); //create div to attach to
+    newTab.classList.add("books");
+    library.appendChild(newTab);
+    newTab.innerText+=`${book}`;
+    // libraryTable.insertRow(libraryTable[myLibrary.indexOf(book)]);
+}
+
