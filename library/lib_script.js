208620be11effb33ddda5e5be4688330a96df7a8
long <17272827+longb1@users.noreply.github.com>
2022-05-01 16:35:54 +0100

    add comments to code
    add comments to code which assist me in knowing what it does.

--- library/lib_script.js
+++ library/lib_script.js
@@ -1,14 +1,17 @@
 const addBookBtn=document.getElementById("bookBtn");
 const overlay = document.getElementById("overlay"); /*grey screen of popup */
 const popup=document.getElementById("popupWindow");
+const closePopupBtn=document.getElementById("closePopup");
 const library = document.getElementById("container"); /*where books are displayed */
 const submitFormBtn= document.getElementById("submitForm");
 
 let myLibrary = []; 
 
-addBookBtn.addEventListener('click',()=>{launchPopup(popup)});
+//Display Dialog Box Functionality
 
-overlay.addEventListener('click',()=>{closePopup(popup,overlay)});
+addBookBtn.addEventListener('click',()=>{launchPopup(popup)});
+overlay.addEventListener('click',()=>{closePopup(popup)});
+closePopupBtn.addEventListener('click',()=>{closePopup(popup)});
 
 function launchPopup(window){
   if (window==null) return; //if no window element found, then exit function.
@@ -16,11 +19,13 @@ function launchPopup(window){
   overlay.classList.add("active");
 }
 
-function closePopup(window,overlay){
+function closePopup(window){
   if (window==null) return //if no window element found, then exit function.
   window.classList.remove("active")
   overlay.classList.remove("active")
 }
+
+//Display Book Functionality
 
 class Book {
   constructor(author, title, pageNumber, hasBeenRead) {
@@ -29,18 +34,24 @@ class Book {
     this.pageNumber = pageNumber;
     this.hasBeenRead = hasBeenRead;
   }
+
+  reading(){
+    if(this.hasBeenRead =true){
+      this.hadBeenRead==false;
+    }else{
+      this.hasBeenRead==true;
+    }
+  }
 }
 
 submitFormBtn.addEventListener('click',submitForm);
 
-
-function submitForm(e){
+function submitForm(e){ //Take form inputs
   e.preventDefault();
   const author = document.querySelector("[data-bookAuthor]").value
   const title = document.querySelector("[data-bookTitle]").value
   const pageNumber= document.querySelector("[data-bookPages]").value
-  const hasBeenRead= document.querySelector("[data-hasRead]").value
-
+  const hasBeenRead= document.querySelector("[data-hasRead]").checked
   const newBook = new Book(author,title,pageNumber,hasBeenRead);
   myLibrary.push(newBook);
 
@@ -49,18 +60,18 @@ function submitForm(e){
 }
 
 
-function displayBookOnPage(author,title,pageNumber,hasBeenRead){
-    let newTab= document.createElement("div"); //create div to attach to
-    newTab.classList.add("books");
-    newTab.innerText+=`${author} ${title} ${pageNumber} ${hasBeenRead}`;
+function displayBookOnPage(author,title,pageNumber,hasBeenRead){ //display form input on page
+  let newTab= document.createElement("div"); //create div to attach to
+  newTab.classList.add("books");
+  newTab.innerText+=`Author:${author} Title:${title} PageNum:${pageNumber} Read?:${hasBeenRead}`;
 
-    let removeBtn=document.createElement("button"); //create button
-    removeBtn.textContent="Remove Book"; //button text
-    removeBtn.addEventListener("click",()=>{ //event listernet
-      newTab.remove()
-    })
+  let removeBtn=document.createElement("button"); //button to remove book
+  removeBtn.textContent="Remove";
+  removeBtn.addEventListener("click",()=>{
+    newTab.remove()
+  })
 
-    library.appendChild(newTab);
-    newTab.appendChild(removeBtn)
+  library.appendChild(newTab);
+  newTab.appendChild(removeBtn)
     
 }
