a46ab486aea5803ea41ff5a9e55374d4c288d84f
Long <17272827+longb1@users.noreply.github.com>
2022-05-27 14:48:33 +0100

    initial commit new project
    add files for weather forecaster project. no code yet

--- gif-generator/gif_script.js
+++ gif-generator/gif_script.js
@@ -10,16 +10,30 @@ function submitForm(e){ //Take form inputs
   getGif(gifValue)
 }
 
+//old syntax to handle APIs (which are promises)
 
-function getGif(value){
-    fetch(`https://api.giphy.com/v1/gifs/translate?api_key=UF5VFsIKYq2eDsJIc0U0N9yziPKoIsPD&s=${value}`, {mode: 'cors'})
-        .then(function(response) {
-            return response.json();
-        })
-        .then(function(response) {
-            img.src = response.data.images.original.url;
-        })
-        .catch(function(error){
-            throw new Error(error);
-        })
+// function getGif(value){
+//     fetch(`https://api.giphy.com/v1/gifs/translate?api_key=UF5VFsIKYq2eDsJIc0U0N9yziPKoIsPD&s=${value}`, {mode: 'cors'})
+//         .then(function(response) {
+//             return response.json();
+//         })
+//         .then(function(response) {
+//             img.src = response.data.images.original.url;
+//         })
+//         .catch(function(error){
+//             throw new Error(error);
+//         })
+// }
+
+//async/await is syntactical sugar for promises, see below
+
+async function getGif(value){
+    try{
+        const gifAPI = await fetch(`https://api.giphy.com/v1/gifs/translate?api_key=UF5VFsIKYq2eDsJIc0U0N9yziPKoIsPD&s=${value}`, {mode: 'cors'})
+        const gifData= await gifAPI.json();
+        img.src = gifData.data.images.original.url;
+    }catch(error){
+        throw new Error(error);
+    }
+    
 }
---
+++ weather-app/weather_script.js
@@ -0,0 +1,9 @@
+// async function getPersonsInfo(name) {
+//     try {
+//       const people = await server.getPeople();
+//       const person = people.find(person => { return person.name === name });
+//       return person;
+//     } catch (error) {
+//       // Handle the error any way you'd like
+//     }
+//   }
