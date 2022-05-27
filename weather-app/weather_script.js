79e4ee258bdcaa111d5f81819769a1001c8c656e
long <17272827+longb1@users.noreply.github.com>
2022-05-27 18:51:59 +0100

    complete API implementation
    complete API implementation to give the temperature when you search for
    a location.

--- weather-app/weather_script.js
+++ weather-app/weather_script.js
@@ -1,9 +1,28 @@
-// async function getPersonsInfo(name) {
-//     try {
-//       const people = await server.getPeople();
-//       const person = people.find(person => { return person.name === name });
-//       return person;
-//     } catch (error) {
-//       // Handle the error any way you'd like
-//     }
-//   }
+const forecastContainer=document.getElementById("forecast")
+const submitFormBtn=document.querySelector('[data-submit-btn]')
+submitFormBtn.addEventListener('click',submitForm);
+
+function submitForm(e){ //Take form inputs
+    e.preventDefault();
+    const forecastLocation = document.querySelector("[data-location]").value
+    const forecastUnit= document.querySelector("[data-unit]").value
+    document.forms[0].reset(); //wipe form inputs
+    getWeather(forecastLocation, forecastUnit)
+}
+
+
+async function getWeather(location, unit){
+    try{
+        forecastContainer.innerHTML='loading...';
+
+        const weatherAPI = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${location}&units=${unit}&APPID=a5d36dfa6b83adaf401640da392db25c`, {mode: 'cors'})
+        const weatherData= await weatherAPI.json();
+        const temperature=weatherData.main.temp
+        forecastContainer.textContent=`The temperature is ${weatherData.main.temp} ${unit}`;
+
+    }catch(error){
+        forecastContainer.textContent='Please enter a valid location!';
+        throw new Error(error);
+    }
+    
+}
